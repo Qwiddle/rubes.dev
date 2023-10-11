edd9acb61b9104d6b32bfb94bca0f95ead33f020
Andrew Rubenstein <andjruben@gmail.com>
2023-10-10 21:11:32 -0400

    feat: add social links to footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,9 +1,29 @@
+import { Twitter, Github } from "lucide-react";
+
 export const SiteFooter = () => {
   return (
-    <div className="">
-      <p className="text-sm text-muted-foreground">
+    <div className="flex justify-between items-center">
+      <p className="text-xs font-medium text-muted-foreground">
         &copy; 2021 - {new Date().getFullYear()} Andrew Rubenstein
       </p>
+      <div className="sm:hidden md:flex gap-4 text-muted-foreground">
+        <a
+          className="flex space-x-2 items-center cursor-pointer hover:hover-text"
+          href="https://twitter.com/TweakerTez"
+          target="about:blank"
+        >
+          <Twitter className="h-6 w-6 text-muted-foreground" />
+          <p className="text-xs font-medium">@tweakertez</p>
+        </a>
+        <a
+          className="flex space-x-2 items-center cursor-pointer hover:hover-text"
+          href="https://github.com/Qwiddle"
+          target="about:blank"
+        >
+          <Github className="h-6 w-6 text-muted-foreground" />
+          <p className="text-xs font-medium">@qwiddle</p>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
