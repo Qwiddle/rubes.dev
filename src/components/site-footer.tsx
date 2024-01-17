1c933dfc05b89bcfa4ae76ccf978cb91759eb1d4
Andrew R <andjruben@gmail.com>
2024-01-16 21:38:45 -0500

    feat: add padding to footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -2,7 +2,7 @@ import { Twitter, Github } from "lucide-react";
 
 export const SiteFooter = () => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center pb-4">
       <p className="text-xs font-medium text-muted-foreground">
         &copy; 2021 - {new Date().getFullYear()} Andrew Rubenstein
       </p>
@@ -12,16 +12,16 @@ export const SiteFooter = () => {
           href="https://twitter.com/TweakerTez"
           target="about:blank"
         >
-          <Twitter className="h-5 w-5 text-muted-foreground" />
-          <p className="text-xs font-medium">@tweakertez</p>
+          <Twitter className="h-4 w-4 text-muted-foreground" />
+          <p className="text-xs font-medium underline">@tweakertez</p>
         </a>
         <a
           className="flex space-x-2 items-center cursor-pointer hover:hover-text"
           href="https://github.com/Qwiddle"
           target="about:blank"
         >
-          <Github className="h-5 w-5 text-muted-foreground" />
-          <p className="text-xs font-medium">@qwiddle</p>
+          <Github className="h-4 w-4 text-muted-foreground" />
+          <p className="text-xs font-medium underline">@qwiddle</p>
         </a>
       </div>
     </div>
